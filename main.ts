f2da2b79ab9215434a6e3bb4b6c59af7852e2954
J-to-go <70455541+Jey-to-go@users.noreply.github.com>
2020-09-04 13:46:14 +0900

    ぐーちょきぱー

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showNumber(randint(0, 10))
+let グーチョキパー = 0
+input.onGesture(Gesture.Shake, function () {
+    グーチョキパー = randint(0, 2)
+    if (グーチョキパー == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (グーチョキパー == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
